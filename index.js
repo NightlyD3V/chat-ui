2d4687c6ef28ced64b27cb24d6ce5de51670c1f7
NightlyD3V <TylerSpaulding95@gmail.com>
2026-03-01 02:01:11 -0500

    Working on chat functionality

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 // CHAT
-let messages = [];
-
 const socket = io("https://chat-server-3bcx.onrender.com");
-socket.on("chat message", msg => {
-  console.log("New message:", msg);
+
+// HANDLE MESSAGES
+let messages = [];
+const sendBtn = document.getElementById('submit');
+// SEND
+sendBtn.addEventListener('click', () => {
+  const msg = input.value.trim();
+  if (msg) {
+    socet.emit('chat message', msg); 
+    input.value = '';
+  }
+});
+//RECIEVE 
+socket.on('chat message', (msg) => {
+  const msgDiv = document.createElement('div');
+  msgDiv.textContent = msg;
+  msgDiv.style.padding = '5px';
+  msgDiv.style.borderBottom = '1px solid #eee';
+  messagesDiv.appendChild(msgDiv);
+  chatPopup.scrollTop = chatPopup.scrollHeight;
 });
 
-function sendMessage(msg) {
-  socket.emit("chat message", msg);
-}
-
 // HANDLE EXIT (CROSS-ORIGIN)
-const button = document.getElementById("exit-button");
+const button = document.getElementById('exit-button');
 
-button.addEventListener("click", () => {
+button.addEventListener('click', () => {
   console.log("You clicked the exit button!");
   window.parent.postMessage("exit-button_clicked", "https://bitshift.neocities.org/");
 });
